16ee5a644793e92c72d5b46aacb6294712b2e2cc
david-cl1 <crowndavid32@gmail.com>
2026-05-03 11:58:58 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form")
+  const taskList = document.getElementById("tasks")
+
+ form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const taskDescription = event.target["new-task-description"].value;
+    buildToDo(taskDescription);
+    })
+
+  function buildToDo(task) {
+      const li = document.createElement("li");
+      
+      li.textContent = task;
+      
+      taskList.appendChild(li);
+  }})
